6e9649f83449c7cc4758d72c716c66df3b95a97c
Nathan HANEN <hanen.nathan@gmail.com>
2022-01-19 22:07:30 +0100

    better base window style

---
+++ NCPanel/CustomWindowStyle.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NCPanel
+{
+    /// <summary>
+    /// Logique d'interaction pour WindowStyle.xaml
+    /// </summary>
+    public partial class CustomWindowStyle : ResourceDictionary
+    {
+        public static readonly DependencyProperty HideTitleBarProperty = DependencyProperty.RegisterAttached("HideTitleBar", typeof(bool), typeof(CustomWindowStyle), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public static bool GetHideTitleBar(DependencyObject element)
+        {
+            return (bool)element.GetValue(HideTitleBarProperty);
+        }
+
+        public static void SetHideTitleBar(DependencyObject element, bool value)
+        {
+            element.SetValue(HideTitleBarProperty, value);
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is DependencyObject obj)
+                Window.GetWindow(obj).Close();
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is DependencyObject obj)
+            {
+                var window = Window.GetWindow(obj);
+                window.WindowState = window.WindowState switch
+                {
+                    WindowState.Normal => WindowState.Maximized,
+                    _ => WindowState.Normal,
+                };
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is DependencyObject obj)
+                Window.GetWindow(obj).WindowState = WindowState.Minimized;
+        }
+    }
+}
